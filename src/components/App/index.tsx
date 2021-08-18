e665a5e6638ff652f481d2a1283bf9bb4e637574
Kanstantsin Haradnichuk <K.Haradnichuk@itechart-group.com>
2021-08-18 12:52:34 +0300

    add window size by useEffect on container

---
+++ src/components/App/index.tsx
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from "react";
+
+import "./index";
+
+interface IWindowSize {
+  width: number;
+  height: number;
+}
+
+const App: React.FC = () => {
+  const [windowSize, setWindowSize] = useState<IWindowSize>({
+    width: 0,
+    height: 0,
+  });
+
+  useEffect(() => {
+    setWindowSize({
+      width: window.innerWidth,
+      height: window.innerHeight,
+    });
+  }, []);
+
+  return (
+    <div
+      className="app_container"
+      style={{
+        height: windowSize.height,
+      }}
+    >
+      <div className="app_title">App</div>
+    </div>
+  );
+};
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-import 'index.scss';
+import "index.scss";
+import App from "components/App";
 
-const App: React.FC = () => {
- return (
-     <div>App</div>
- );
-}
-
-ReactDOM.render(<App/>,document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
